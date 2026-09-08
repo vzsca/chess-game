ebe96487b9e72750e66286f79ffc67dc38ca07cb
vzsca <lord.vesca@gmail.com>
2026-09-08 18:47:12 +0200

    Refactor: add new application launcher

---
+++ app.py
@@ -0,0 +1,14 @@
+"""Compatibility application launcher."""
+from tkinter import Tk
+from frontend.ui import ChessUI
+
+
+def main():
+    root = Tk()
+    root.title("Chess Game")
+    ChessUI(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
